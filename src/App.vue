549c053cec9cdacf145ff654e2eb250fcd3fa464
Alexander <misochenkoas@gmail.com>
2022-05-18 17:59:40 +0300

    feat: add base styles to project + started todo block

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    123
+  <div class="todo">
+    <img src="./assets/todo-img.svg" alt="todo-img" class="todo__img">
   </div>
 </template>
 <script lang="ts">
@@ -9,5 +9,18 @@ export default {
 }
 </script>
 <style lang="scss">
-  
+  @import "./assets/style/base.scss";
+
+  .todo {
+    max-width: 730px;
+    width: 100%;
+    margin: 0 15px;
+    padding: 32px;
+    background: #fff;
+    border-radius: 24px;
+    &__img {
+      display: block;
+      margin: 0 auto;
+    }
+  }
 </style>
